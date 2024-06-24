b2510cd355040146d1c68815a57ee007a5f23462
Alina Salakhova <alina.salakhova1991@gmail.com>
2024-06-24 19:14:27 +0200

    Completed hw_24.2 Task 2

--- users/models.py
+++ users/models.py
@@ -1,6 +1,8 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+from materials.models import Course, Lesson
+
 
 class User(AbstractUser):
     """
@@ -30,3 +32,37 @@ class User(AbstractUser):
 
     def __str__(self):
         return self.email
+
+
+class Payment(models.Model):
+    """
+    Модель для хранения информации о платежах пользователей.
+
+    Attributes:
+        user (ForeignKey): Ссылка на пользователя, который совершил платеж.
+        payment_date (DateTimeField): Дата и время платежа.
+        paid_course (ForeignKey): Ссылка на оплаченный курс (может быть пустым).
+        paid_lesson (ForeignKey): Ссылка на оплаченный урок (может быть пустым).
+        amount (DecimalField): Сумма платежа.
+        payment_method (CharField): Способ оплаты (наличные или перевод на счет).
+    """
+    PAYMENT_METHOD_CHOICES = [
+        ('cash', 'Наличные'),
+        ('transfer', 'Перевод на счет'),
+    ]
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name="Пользователь")
+    payment_date = models.DateTimeField(auto_now_add=True, verbose_name="Дата оплаты")
+    paid_course = models.ForeignKey(Course, on_delete=models.CASCADE, null=True, blank=True,
+                                    verbose_name="Оплаченный курс")
+    paid_lesson = models.ForeignKey(Lesson, on_delete=models.CASCADE, null=True, blank=True,
+                                    verbose_name="Оплаченный урок")
+    amount = models.DecimalField(max_digits=20, decimal_places=2, verbose_name="Сумма оплаты")
+    payment_method = models.CharField(max_length=20, choices=PAYMENT_METHOD_CHOICES, verbose_name="Способ оплаты")
+
+    class Meta:
+        verbose_name = "Платеж"
+        verbose_name_plural = "Платежи"
+
+    def __str__(self):
+        return f'{self.user} - {self.amount} - {self.payment_method}'
